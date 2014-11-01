46ed31972028ee1b4aacd1e10b5e9f883ca4c464
Carles Jove i Buxeda <hola@carlus.cat>
2014-11-01 22:56:33 +0100

    test passes

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,2 +1,14 @@
 class Plan < ActiveRecord::Base
+  before_validation :generate_slug
+  
+  # Validations
+  validates :name, :price, :slug, presence: true
+
+  private
+
+    def generate_slug
+      # Not calling to_s throws a weird NOMethodError for NilClass
+      # https://github.com/rails-api/rails-api/issues/164
+      self.slug = name.gsub(/( )/, '_').downcase
+    end
 end
--- test/models/plan_test.rb
+++ test/models/plan_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class PlanTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def test_that_a_slug_is_generated_by_underscoring_name
+    plan = Plan.new(name: 'My New Plan', price: plans(:one).price)
+
+    assert plan.valid?
+    assert plan.save
+    assert_equal 'my_new_plan', plan.slug
+  end
 end
